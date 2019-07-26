d7b0419d9856264f5619559aff28906ee0a7f2f4
Yang Wang <yang.wang@oit.edu>
2019-07-26 13:10:06 -0700

    Added GUI displays + Key Generation

--- SDES/SDES/MainPage.xaml.cs
+++ SDES/SDES/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,10 +23,30 @@ namespace SDES
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private String keyOne;
+        private String keyTwo;
         public MainPage()
         {
             this.InitializeComponent();
         }
 
+        private void GenerateKeyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var generateKey = new KeyGenerator();
+
+            var key = generateKey.GenerateKeys(CreateKeyBox.Text);
+            keyOne = key.Substring(0, 8);
+            keyTwo = key.Substring(8, 8);
+
+            KeyOne.Text = keyOne;
+            KeyTwo.Text = keyTwo;
+        }
+
+        private void GenerateEncryptTextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var encrypt = new EncryptionSDES();
+
+            CipherText.Text = encrypt.EncryptionMode(keyOne, keyTwo, PlainTextBox.Text);
+        }
     }
 }
